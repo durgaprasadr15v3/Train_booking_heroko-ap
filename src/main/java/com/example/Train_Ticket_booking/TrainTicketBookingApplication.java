03ff73fd5e5e7f2e631be9077f783b594a520001
durgaprasadr15v3 <durgaprasadr15v3@gmail.com>
2024-02-11 23:02:25 +0530

    first project

---
+++ src/main/java/com/example/Train_Ticket_booking/TrainTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Train_Ticket_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Train_Ticket_booking/TrainTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Train_Ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
